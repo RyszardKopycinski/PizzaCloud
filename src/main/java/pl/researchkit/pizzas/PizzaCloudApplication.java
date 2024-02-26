0ce24683df32f190eb7919cbacafb76be4a22ae0
Ryszard <ryszardkopycinski13@gmail.com>
2024-02-26 12:54:34 +0100

    Start

--- src/main/java/pl/researchkit/pizzas/PizzaCloudApplication.java
+++ src/main/java/pl/researchkit/pizzas/PizzaCloudApplication.java
@@ -1,13 +1,15 @@
 package pl.researchkit.pizzas;
 
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootApplication
 public class PizzaCloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaCloudApplication.class, args);
 	}
-
 }
