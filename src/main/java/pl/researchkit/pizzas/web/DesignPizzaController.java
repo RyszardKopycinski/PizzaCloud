6b6705eb7f4a2b9709dc6bf3b110859f83685e33
Ryszard <ryszardkopycinski13@gmail.com>
2024-03-02 20:20:32 +0100

    design 1.0

--- src/main/java/pl/researchkit/pizzas/web/DesignPizzaController.java
+++ src/main/java/pl/researchkit/pizzas/web/DesignPizzaController.java
@@ -3,8 +3,6 @@ package pl.researchkit.pizzas.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -72,11 +70,9 @@ public class DesignPizzaController {
     }
 
     @PostMapping
-    public String processDEsign(@Validated @ModelAttribute("design") Pizza design, Errors errors, Model model) {
-        if (errors.hasErrors()) {
-            return "design";
-        }
-        log.info("Processing design: " + design);
+    public String processDesign(Pizza pizzaDesign) {
+
+        log.info("Przetwarzanie projektu pizza: " + pizzaDesign);
         return "redirect:/orders/current";
     }
 
