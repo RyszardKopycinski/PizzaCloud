024706862f35e3de4897ad0c3cddd0f40f232602
Ryszard <ryszardkopycinski13@gmail.com>
2024-03-03 10:11:38 +0100

    orderForm 1.0

--- src/main/java/pl/researchkit/pizzas/web/OrderController.java
+++ src/main/java/pl/researchkit/pizzas/web/OrderController.java
@@ -4,16 +4,24 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.researchkit.pizzas.Order;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 public class OrderController {
 
-    @GetMapping
+    @GetMapping("current")
     public String orderForm(Model model) {
         model.addAttribute("order", new Order());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order) {
+        log.info("Zamówienie zostało złożone: " + order);
+        return "redirect:/";
     }
 }
